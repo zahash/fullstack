0e1e8ee22b6a7ee1fde3f94dc30d98313f2716f7
zahash <zahash.z@gmail.com>
2024-10-27 22:02:29 +0530

    refactor duplicate code

--- test-proc-macros/src/lib.rs
+++ test-proc-macros/src/lib.rs
@@ -1,46 +1,30 @@
 #[cfg(feature = "username")]
 #[proc_macro]
 pub fn username(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    use std::str::FromStr;
-
-    let lit_str = syn::parse_macro_input!(input as syn::LitStr);
-    let value = lit_str.value();
-
-    if let Err(err) = server::Username::from_str(&value) {
-        return syn::Error::new(lit_str.span(), err)
-            .to_compile_error()
-            .into();
-    }
-
-    quote::quote! { #value }.into()
+    domain_type_compiletime_check::<server::Username>(input)
 }
 
 #[cfg(feature = "password")]
 #[proc_macro]
 pub fn password(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    use std::str::FromStr;
-
-    let lit_str = syn::parse_macro_input!(input as syn::LitStr);
-    let value = lit_str.value();
-
-    if let Err(err) = server::Password::from_str(&value) {
-        return syn::Error::new(lit_str.span(), err)
-            .to_compile_error()
-            .into();
-    }
-
-    quote::quote! { #value }.into()
+    domain_type_compiletime_check::<server::Password>(input)
 }
 
 #[cfg(feature = "email")]
 #[proc_macro]
 pub fn email(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    use std::str::FromStr;
+    domain_type_compiletime_check::<server::Email>(input)
+}
 
+fn domain_type_compiletime_check<T>(input: proc_macro::TokenStream) -> proc_macro::TokenStream
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Display,
+{
     let lit_str = syn::parse_macro_input!(input as syn::LitStr);
     let value = lit_str.value();
 
-    if let Err(err) = server::Email::from_str(&value) {
+    if let Err(err) = T::from_str(&value) {
         return syn::Error::new(lit_str.span(), err)
             .to_compile_error()
             .into();
