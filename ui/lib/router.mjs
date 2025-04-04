aacfef0f17b5dd5f5022a3847b4e22eb0a8f3890
zahash <zahash.z@gmail.com>
2025-04-04 17:38:28 +0530

    update router

--- ui/lib/router.mjs
+++ ui/lib/router.mjs
@@ -1,2 +1,2 @@
-export { default } from "https://cdn.jsdelivr.net/gh/zahash/spamf@4fb2cdf7338861cdab62af4857239e8d03cd46d4/router.js";
-export * from "https://cdn.jsdelivr.net/gh/zahash/spamf@4fb2cdf7338861cdab62af4857239e8d03cd46d4/router.js";
+export { default } from "https://cdn.jsdelivr.net/gh/zahash/spamf@a4700b80d4ff9f733cde99e54d966e5243a6c9f2/router.js";
+export * from "https://cdn.jsdelivr.net/gh/zahash/spamf@a4700b80d4ff9f733cde99e54d966e5243a6c9f2/router.js";
