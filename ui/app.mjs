f2489b321e0f2bfd96e434b575431b3be145abd8
zahash <zahash.z@gmail.com>
2025-04-07 08:18:08 +0530

    include script and styles in template itself instead of in router

--- ui/app.mjs
+++ ui/app.mjs
@@ -1,10 +1,11 @@
 import initRouter from "./lib/router.mjs";
 
 export const hooks = initRouter({
-    "/": { template: "./pages/home/home.html", title: "Home", scripts: ["./pages/home/home.js"] },
-    "/login": { template: "./pages/login/login.html", title: "Login", scripts: ["./pages/login/login.mjs"] },
-    "/signup": { template: "./pages/signup/signup.html", title: "Sign Up", scripts: ["./pages/signup/signup.mjs", "./pages/signup/lifecycle.js"] }
-}, {
+    routes: {
+        "/": { template: "./pages/home/home.html" },
+        "/login": { template: "./pages/login/login.html" },
+        "/signup": { template: "./pages/signup/signup.html" }
+    },
     fragments: {
         header: "./fragments/header.html",
         nav: "./fragments/nav.html",
--- ui/lib/router.mjs
+++ ui/lib/router.mjs
@@ -1,2 +1,2 @@
-export { default } from "https://cdn.jsdelivr.net/gh/zahash/spamf@a4700b80d4ff9f733cde99e54d966e5243a6c9f2/router.js";
-export * from "https://cdn.jsdelivr.net/gh/zahash/spamf@a4700b80d4ff9f733cde99e54d966e5243a6c9f2/router.js";
+export { default } from "https://cdn.jsdelivr.net/gh/zahash/spamf@46d8fa43ed02c189c719a5a39508d1e0c29b7adb/router.js";
+export * from "https://cdn.jsdelivr.net/gh/zahash/spamf@46d8fa43ed02c189c719a5a39508d1e0c29b7adb/router.js";
