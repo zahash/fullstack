0aca784d42b1c9c174da0806238b8159af7fb79e
zahash <zahash.z@gmail.com>
2025-04-09 10:46:37 +0530

    fix request macro to support empty body (unit type)

--- server/tests/shared/macros.rs
+++ server/tests/shared/macros.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! request {
     ( $method:ident $url:expr ; $($header:expr => $value:expr)* ; $($body:expr)? ) => {{
+        #[allow(unused_mut)]
         let mut req = axum::http::Request::builder()
             .method(stringify!($method))
             .uri($url);
@@ -9,7 +10,7 @@ macro_rules! request {
             req = req.header($header, $value);
         )*
 
-        req.body(axum::body::Body::from($( $body )?)).expect("unable to build request")
+        req.body( axum::body::Body::from(( $( $body )? )) ).expect("unable to build request")
     }};
 }
 
