88e56c172270c816bf7a5c7f9e636846d81a4a2a
zahash <zahash.z@gmail.com>
2025-05-20 16:45:37 +0530

    update private url

--- api/src/private.rs
+++ api/src/private.rs
@@ -1,9 +1,8 @@
-use server_core::{InsufficientPermissionsError, Principal};
+use server_core::Principal;
 
 #[tracing::instrument(fields(user_id = tracing::field::Empty), skip_all, ret)]
-pub async fn private(principal: Principal) -> Result<String, InsufficientPermissionsError> {
+pub async fn private(principal: Principal) -> String {
     let user_id = principal.user_id();
     tracing::Span::current().record("user_id", &tracing::field::debug(user_id));
-
-    Ok(format!("hello {:?}", user_id))
+    format!("hello {:?}", user_id)
 }
