e5e8d1da049faa29262b97866b61dd9a241470e0
zahash <zahash.z@gmail.com>
2025-08-21 20:21:25 +0530

    make access token name unique per user instead of unique globally

--- migrations/20241015141500_create_access_tokens_table.sql
+++ migrations/20241015141500_create_access_tokens_table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE access_tokens (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
     access_token_hash BLOB NOT NULL UNIQUE,
     user_id INTEGER NOT NULL,
     created_at DATETIME NOT NULL,
     expires_at DATETIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    UNIQUE (user_id, name)
 );
