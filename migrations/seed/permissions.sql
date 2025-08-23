f700ecbcdff04f633e49c38eae75314f707b08fd
zahash <zahash.z@gmail.com>
2025-08-23 11:09:42 +0530

    update permissions seed

--- migrations/seed/permissions.sql
+++ migrations/seed/permissions.sql
@@ -6,22 +6,36 @@ INSERT INTO permissions (permission, description) VALUES
 ON CONFLICT (permission) DO NOTHING;
 
 
-INSERT INTO permission_groups (group, description) VALUES
-('signup',      'for users that just signed up'),
-('admin',       'for site administrators')
-ON CONFLICT (group) DO NOTHING;
+INSERT INTO permission_groups ([group], description) VALUES
+('root',        'for superuser access'),
+('admin',       'for site administrators'),
+('signup',      'for users that just signed up')
+ON CONFLICT ([group]) DO NOTHING;
 
 
-WITH mapping(permission, [group]) AS (
+WITH mapping([group], permission) AS (
   VALUES
-    ('post:/access-token/generate',         'signup'),
-    ('get:/permissions',                    'admin'),
-    ('post:/permissions/assign',            'admin'),
-    ('get:/sysinfo',                        'admin')
+    ('signup',    'post:/access-token/generate'),
+    ('signup',    'get:/permissions'),
+    ('signup',    'post:/permissions/assign'),
+
+    ('admin',     'post:/access-token/generate'),
+    ('admin',     'get:/permissions'),
+    ('admin',     'post:/permissions/assign'),
+    ('admin',     'get:/sysinfo')
 )
 INSERT INTO permission_group_association (permission_id, permission_group_id)
 SELECT p.id, pg.id
 FROM mapping m
 INNER JOIN permissions p ON p.permission = m.permission
 INNER JOIN permission_groups pg ON pg.[group] = m.[group]
+ON CONFLICT (permission_id, permission_group_id) DO NOTHING;
+
+
+-- Assign ALL permissions to the 'root' group
+INSERT INTO permission_group_association (permission_id, permission_group_id)
+SELECT p.id, pg.id
+FROM permissions p
+CROSS JOIN permission_groups pg
+WHERE pg.[group] = 'root'
 ON CONFLICT (permission_id, permission_group_id) DO NOTHING;
