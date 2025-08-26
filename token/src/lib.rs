5f45f85e752ed537fb38259d8d5aa025e2104618
zahash <zahash.z@gmail.com>
2025-08-26 16:44:36 +0530

    zeroize Token bytes

--- token/src/lib.rs
+++ token/src/lib.rs
@@ -6,32 +6,33 @@ use std::fmt::Display;
 use base64::{Engine, prelude::BASE64_URL_SAFE_NO_PAD};
 use rand::RngCore;
 use sha2::{Digest, Sha256};
+use zeroize::Zeroizing;
 
 #[derive(Debug, Clone)]
-pub struct Token<const N: usize>([u8; N]);
+pub struct Token<const N: usize>(Zeroizing<[u8; N]>);
 
 impl<const N: usize> Token<N> {
     pub fn random() -> Self {
         let mut rng = rand::rng();
         let mut buffer = [0u8; N];
         rng.fill_bytes(&mut buffer);
-        Self(buffer)
+        Self(Zeroizing::new(buffer))
     }
 
     pub fn hash_sha256(&self) -> Vec<u8> {
         let mut hasher = Sha256::new();
-        hasher.update(self.0);
+        hasher.update(&self.0);
         hasher.finalize().to_vec()
     }
 
     pub fn base64encoded(&self) -> String {
-        BASE64_URL_SAFE_NO_PAD.encode(self.0)
+        BASE64_URL_SAFE_NO_PAD.encode(&self.0)
     }
 
     pub fn base64decode(s: &str) -> Result<Self, &str> {
         let bytes = BASE64_URL_SAFE_NO_PAD.decode(s).map_err(|_| s)?;
         let bytes: [u8; N] = bytes.try_into().map_err(|_| s)?;
-        Ok(Token(bytes))
+        Ok(Self(Zeroizing::new(bytes)))
     }
 
     #[inline]
@@ -40,7 +41,7 @@ impl<const N: usize> Token<N> {
     }
 
     #[inline]
-    pub fn into_bytes(self) -> [u8; N] {
+    pub fn into_bytes(self) -> Zeroizing<[u8; N]> {
         self.into()
     }
 }
@@ -48,11 +49,11 @@ impl<const N: usize> Token<N> {
 impl<const N: usize> From<[u8; N]> for Token<N> {
     #[inline]
     fn from(bytes: [u8; N]) -> Self {
-        Token(bytes)
+        Self(Zeroizing::new(bytes))
     }
 }
 
-impl<const N: usize> From<Token<N>> for [u8; N] {
+impl<const N: usize> From<Token<N>> for Zeroizing<[u8; N]> {
     #[inline]
     fn from(token: Token<N>) -> Self {
         token.0
